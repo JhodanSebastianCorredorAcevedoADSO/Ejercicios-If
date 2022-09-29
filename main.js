9d3502ad65f56849bd7e7ebc3fac5031a2def987
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-28 22:21:10 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-/// Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-/// ¿Qué figura quiere calcular (Escriba T o C)?
-/// Triangulo = base * altura / 2
-/// Circulo = PI * radio* radio
+/// Escriba un algoritmo que calcule el área de un rectángulo siempre
+/// y cuando los lados sean positivos: área triangulo= lado * lado.
 
-/// elijimos que vamos a calcular
+/// pedimos el valor
+let lado = Number(prompt("Ingresar el lado del rectangulo "))
 
-let fig = prompt("calcular la siguiente figura circulo(C) o triangulo (T): ")
+if(lado){
 
-let pi = 3.14
-
-/// si la figura que se escogia es igual a T
-
-if(fig == "T"){
-    
-    b = Number(prompt("la base es: "))
-    a = Number(prompt("la altura es: "))
-
-    ///calcular el valor
-    let resul1 = (b*a)/2
-
-    console.log("el area del triangulo es: ", resul1)
-
-
-/// si la figura que se escogia es igual a C
-}else if(fig == "C"){
-    r = Number(prompt("el radio del circulo es: "))
-
-    /// calcular el valor
-    let resul2 = pi * r**2
-
-    console.log("el area del circulo es:", resul2 )
-
-}else{
-    
-    console.log("lo ingresado no es valido")
-    /// impresion para no validar
+    /// operacion y resultado
+    let resul1 = lado * lado
+    console.log("el lado del rectangulo es :", resul1)
 
 }
-
 })
