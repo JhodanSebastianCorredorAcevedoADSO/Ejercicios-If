f5893346c7a51cfc58531613f81b237495980a3d
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-28 23:20:51 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-// Restarle al primer número al segundo (siempre y cuando el primero
-/// sea mayor que él segundo, en caso contrario indicar con un mensaje 
-/// que la operación no es posible realizarla
+/// En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+/// de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+/// Los descuentos son los siguientes:
+/// Tipo A 10% de descuento
+/// Tipo B 15% de descuento
+/// Tipo C 20% de descuento
 
-/// pedimos datos
+/// damos a eligir lo quie se necesita
 
-let num1 = Number(prompt("ingrese el primer numero:")) 
-let num2 = Number(prompt("ingrese el segundo numero:"))
+let mem = prompt("tipo de membresia (A) (B) (C): ")
 
-/// si num1 es mayor a num2
+if(mem == "A"){
 
-if (num1 > num2){
+    let a = Number(prompt("ingrese el precio: "))
+
+    let des = a * 0.10
+
+    console.log("imprima el descuento", des )
     
-    let res = num1-num2
-    console.log("resta de los numeros:", res)
+}else if ( mem == "B"){
 
-}else(
+    let b = Number(prompt("ingrese el precio: "))
+    
+    let des2 = b * 0.15
+    
+    console.log("imprima el descuento", des2 )
 
-    console.log("la operación no es posible realizar")
+}if(mem =="C"){
 
-)
+    let c = Number(prompt("ingrese le precio: "))
 
+}
 })
