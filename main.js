5d6a32b6d2075c8e0e5c8de1460d846b30e47c7d
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-28 23:03:18 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-let num=Number(prompt(`coloque un  numero`))
-let num2=Number(prompt(`coloque otro  numero`))
-if (num%2 == 0 && num2%2==0){
-    console.log(`el numero es par`)
+/// Calcular el valor total del valor de 5 productos, el IVA y el subtotal,
+/// visualizar los resultados de: Iva, Subtotal y Total de la compra de los artículos.
+
+/// pedimos valores
+
+let num1 = Number(prompt("valor del producto: "))
+let num2 = Number(prompt("valor del producto: "))
+let num3 = Number(prompt("valor del producto: "))
+let num4 = Number(prompt("valor del producto: "))
+let num5 = Number(prompt("valor del producto: "))
+
+/// valor total
+let sum = num1+num2+num3+num4+num5 
+
+/// si es mayor a cero
+if(sum > 0){
+
+    /// hacemos la operacion
+    let iva = sum*0.10
+    let total=(sum+iva)
+    
+    /// imprimimos su precio deacuerdo a lo pedido
+    
+    console.log("Subtotal: ", sum)
+    console.log("IVA: ",iva )
+    console.log("resultado TOTAL: ", total)
+
 }
-else{
-    console.log(`el numero es impar`)
-}
+
 
 })
