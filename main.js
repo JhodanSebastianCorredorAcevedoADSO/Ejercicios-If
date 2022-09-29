8c60cbd17880346e0cc1c89871c2c67daa3d571e
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-28 22:41:39 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-/// Calcular todos los pagos hechos de un restaurante y que si el consumo 
-/// ingresado excede los $130.000 el descuento será del 15%, de lo 
-/// contrario no hay descuento.
+/// En cierta empresa se les paga a sus trabajadores de la siguiente forma:
+/// si el empleado es de planta, la hora trabajada se le paga a $20000, 
+/// si el empleado es administrativo, la hora trabajada se le paga a $10000. 
+/// Para calcular su pago es necesario conocer el total de horas trabajadas.
 
-/// pedimos los datos que necesitamos
+let plan1 = 20000
 
-let num1 = Number(prompt("ingrese el total "))
+let plan2 = 10000
 
-let num2 = Number(prompt("ingrese el total "))
+// preguntamos que cargo ocupa
 
-let num3 = Number(prompt("ingrese el total "))
+let h = prompt("ingrese si es de administrativo (A) o de planta (P): ")
 
-/// sacamos el total
+let horas1= Number(prompt("ingrese las horas: "))
 
-let sum = num1+num2+num3
-
-/// comparamos si la suma es amyor a 130000
-
-if (sum > 130000){
+if(h=="P"){
     
-    let iva = sum*0.15
-    let des = Number(sum-iva)
+    let resul1 = (horas1*plan1)
 
-    /// imprimimos los valores y descuentos
+    /// impresion de las horas trabajadas
+    console.log("horas trabajadas de planta", resul1)
     
-    console.log("el valor de la suma", sum)
-    console.log("el valor con descuento es:",des)
+}else if(h== "A"){
 
-}
+    let resul2 = (horas1*plan2)
+
+    /// imprimimos lo que necesitamos
+    console.log("horas trabajadas de administracion", resul2)
+
+}  
 
 })
